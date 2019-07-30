cddce1cd03950ca77cfd7e5dcf99b393e9414059
krestaino <kevinrestaino@gmail.com>
2019-07-29 21:50:27 -0500

    refacotr: cleanup ui files

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = {
+    seafile: null,
+    plex: null,
+    unifi: null,
+    netdataDo: null,
+    netdataHome: null,
+    uptimeRobot: null
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  getSeafile() {
+    return axios.get("//localhost:4000/seafile");
+  }
+
+  getPlex() {
+    return axios.get("//localhost:4000/plex");
+  }
+
+  getUnifi() {
+    return axios.get("//localhost:4000/unifi");
+  }
+
+  getNetdataDo() {
+    return axios.get("//localhost:4000/netdata-do");
+  }
+
+  getNetdataHome() {
+    return axios.get("//localhost:4000/netdata-home");
+  }
+
+  getUptimeRobot() {
+    return axios.get("//localhost:4000/uptime-robot");
+  }
+
+  componentDidMount() {
+    axios
+      .all([
+        this.getSeafile(),
+        this.getPlex(),
+        this.getUnifi(),
+        this.getNetdataDo(),
+        this.getNetdataHome(),
+        this.getUptimeRobot()
+      ])
+      .then(
+        axios.spread(
+          (seafile, plex, unifi, netdataDo, netdataHome, uptimeRobot) => {
+            this.setState({ seafile: seafile.data });
+            this.setState({ plex: plex.data });
+            this.setState({ unifi: unifi.data });
+            this.setState({ netdataDo: netdataDo.data });
+            this.setState({ netdataHome: netdataHome.data });
+            this.setState({ uptimeRobot: uptimeRobot.data });
+          }
+        )
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          <li>
+            <pre>{JSON.stringify(this.state.seafile)}</pre>
+          </li>
+          <li>
+            <pre>{JSON.stringify(this.state.plex)}</pre>
+          </li>
+          <li>
+            <pre>{JSON.stringify(this.state.unifi)}</pre>
+          </li>
+          <li>
+            <pre>{JSON.stringify(this.state.netdataDo)}</pre>
+          </li>
+          <li>
+            <pre>{JSON.stringify(this.state.netdataHome)}</pre>
+          </li>
+          <li>
+            <pre>{JSON.stringify(this.state.uptimeRobot)}</pre>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
