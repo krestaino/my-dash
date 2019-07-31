08dd37914bd08a4f23a6d67dba2d9d7147f761f7
krestaino <kevinrestaino@gmail.com>
2019-07-30 23:42:31 -0500

    refactor(plex): code style

--- ui/src/components/Plex.js
+++ ui/src/components/Plex.js
@@ -40,50 +40,47 @@ export default class Plex extends Component {
         ) : (
           <ul>
             {plex.MediaContainer.size > 0 ? (
-              plex.MediaContainer.Metadata.map((stream, index) => {
-                return (
-                  <li className="box mb-8" key={index}>
-                    {stream.grandparentTitle ? (
-                      <div>{stream.grandparentTitle}</div>
-                    ) : (
-                      <div>{stream.title}</div>
+              plex.MediaContainer.Metadata.map((stream, index) => (
+                <li className="box mb-8" key={index}>
+                  {stream.grandparentTitle ? (
+                    <div>{stream.grandparentTitle}</div>
+                  ) : (
+                    <div>{stream.title}</div>
+                  )}
+                  <div className="text-gray-600 text-sm">
+                    {stream.parentIndex && stream.index && (
+                      <span className="pr-2">
+                        S{stream.parentIndex} · E{stream.index} {stream.title}
+                      </span>
                     )}
+                  </div>
+                  {(!stream.parentIndex || !stream.index) && (
+                    <div className="text-gray-600 text-sm">{stream.year}</div>
+                  )}
+                  <div className="text-gray-600 text-sm">
+                    {this.displayTime(stream.duration, "H mm")}
+                  </div>
+                  <div className="flex items-center mt-4 -m-4 p-4 relative border-t">
+                    <img
+                      alt={stream.User.title}
+                      className="rounded-full h-8 w-8 mr-4"
+                      src={stream.User.thumb}
+                    />
                     <div className="text-gray-600 text-sm">
-                      {stream.parentIndex && stream.index && (
-                        <span className="pr-2">
-                          S{stream.parentIndex} · E{stream.index} {stream.title}
-                        </span>
-                      )}
-                    </div>
-                    {(!stream.parentIndex || !stream.index) && (
-                      <div className="text-gray-600 text-sm">{stream.year}</div>
-                    )}
-                    <div className="text-gray-600 text-sm">
-                      {this.displayTime(stream.duration, "H mm")}
-                    </div>
-                    <div className="flex items-center mt-4 -m-4 p-4 relative border-t">
-                      <img
-                        alt={stream.User.title}
-                        className="rounded-full h-8 w-8 mr-4"
-                        src={stream.User.thumb}
-                      />
-                      <div className="text-gray-600 text-sm">
-                        <div>{stream.User.title}</div>
-                        <div>
-                          {stream.Player.device} · {stream.Player.platform}
-                        </div>
+                      <div>{stream.User.title}</div>
+                      <div>
+                        {stream.Player.device} · {stream.Player.platform}
                       </div>
-                      <div
-                        className="absolute bg-blue-500 h-1 inset-x-0 top-0 -mt-1"
-                        style={{
-                          width:
-                            (stream.viewOffset / stream.duration) * 100 + "%"
-                        }}
-                      />
                     </div>
-                  </li>
-                );
-              })
+                    <div
+                      className="absolute bg-blue-500 h-1 inset-x-0 top-0 -mt-1"
+                      style={{
+                        width: (stream.viewOffset / stream.duration) * 100 + "%"
+                      }}
+                    />
+                  </div>
+                </li>
+              ))
             ) : (
               <div className="box text-center text-gray-600 text-sm">
                 No active streams
