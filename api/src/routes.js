b9a6d2d1a8acc3eda57ac658b126adbf98e590f1
krestaino <kevinrestaino@gmail.com>
2019-08-03 16:43:52 -0500

    refactor: arrow function

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 module.exports = (app, authenticate, axios) => {
-  fs.readdirSync(__dirname + "/routes/").forEach(function(file) {
+  fs.readdirSync(__dirname + "/routes/").forEach(file => {
     const fileName = file.substr(0, file.indexOf("."));
     require("./routes/" + fileName)(app, authenticate, axios);
   });
