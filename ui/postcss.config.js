94a7596f26f00ef994e97b3f86a87b299b179d49
krestaino <kevinrestaino@gmail.com>
2019-08-03 12:30:15 -0500

    fix: purgecss

--- ui/postcss.config.js
+++ ui/postcss.config.js
@@ -1,5 +1,5 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.html", "./src/**/*.js"],
+  content: ["./public/**/*.html", "./src/**/*.js"],
 
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
