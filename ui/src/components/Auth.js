a9c5c1640b2f912de5d8e64ca06be5d978918626
krestaino <kevinrestaino@gmail.com>
2019-08-03 18:08:29 -0500

    feat: auth styles

--- ui/src/components/Auth.js
+++ ui/src/components/Auth.js
@@ -17,22 +17,22 @@ export default class Auth extends Component {
 
   render() {
     return (
-      <form className="container mx-auto max-w-lg" onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <div className="box flex mt-8">
           <label className="text-gray-600 dark:text-gray-500 text-sm flex flex-1 items-center">
             <input
               autoFocus
-              className="py-1 px-2 flex flex-1 rounded border dark:border-gray-600 focus:border-gray-600 outline-none bg-transparent"
+              className="py-1 px-2 flex flex-1 rounded border dark:border-gray-600 outline-none bg-transparent"
               type="text"
               value={this.state.API_KEY}
               onChange={this.handleChange}
-              placeholder="API Key"
+              placeholder="API_KEY"
             />
           </label>
           <input
-            className="bg-gray-300 dark:bg-gray-700 text-gray-600 dark:text-gray-500 dark:text-gray-300 text-sm py-1 px-6 rounded ml-2 cursor-pointer"
+            className="bg-gray-300 dark:bg-gray-700 text-gray-600 dark:text-gray-300 dark:text-gray-300 text-sm py-1 px-6 rounded ml-2 cursor-pointer"
             type="submit"
-            value="Submit"
+            value="Connect"
           />
         </div>
       </form>
