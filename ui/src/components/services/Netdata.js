c9a1c9cf306c3923243f8d8b8999be3612942301
krestaino <kevinrestaino@gmail.com>
2019-08-03 19:12:43 -0500

    feat: link to netdata

--- ui/src/components/services/Netdata.js
+++ ui/src/components/services/Netdata.js
@@ -29,12 +29,18 @@ export default class Netdata extends Component {
           {!netdataDo ? (
             <Loading response={netdataDo} />
           ) : (
-            <NetdataInstance server={netdataDo} />
+            <NetdataInstance
+              data={netdataDo}
+              url={process.env.REACT_APP_NETDATA_DO_URL}
+            />
           )}
           {!netdataHome ? (
             <Loading response={netdataHome} />
           ) : (
-            <NetdataInstance server={netdataHome} />
+            <NetdataInstance
+              data={netdataHome}
+              url={process.env.REACT_APP_NETDATA_HOME_URL}
+            />
           )}
         </ul>
       </div>
@@ -44,31 +50,38 @@ export default class Netdata extends Component {
 
 class NetdataInstance extends Component {
   render() {
-    const server = this.props.server;
+    const { data, url } = this.props;
 
     return (
-      <li className="box mb-8" key={server.uid}>
-        <div>{server.mirrored_hosts[0]}</div>
+      <li className="box mb-8" key={data.uid}>
+        <a
+          className="hover:underline"
+          href={url}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {data.mirrored_hosts[0]}
+        </a>
         <div className="text-gray-600 dark:text-gray-500 text-xs">
-          {server.os_name} {server.os_version}
+          {data.os_name} {data.os_version}
         </div>
         <div className="border-t dark:border-gray-700 mt-4 -m-4 p-4">
           <div className="mb-2 text-sm">Alarms</div>
           <div className="text-gray-600 dark:text-gray-500 text-sm justify-between flex w-full">
             <span>Critical</span>
-            <span className={server.alarms.critical && "text-red-600"}>
-              {server.alarms.critical}
+            <span className={data.alarms.critical && "text-red-600"}>
+              {data.alarms.critical}
             </span>
           </div>
           <div className="text-gray-600 dark:text-gray-500 text-sm justify-between flex w-full">
             <span>Warning</span>
-            <span className={server.alarms.warning && "text-yellow-600"}>
-              {server.alarms.warning}
+            <span className={data.alarms.warning && "text-yellow-600"}>
+              {data.alarms.warning}
             </span>
           </div>
           <div className="text-gray-600 dark:text-gray-500 text-sm justify-between flex w-full">
             <span>Normal</span>
-            <span className="text-green-600">{server.alarms.normal}</span>
+            <span className="text-green-600">{data.alarms.normal}</span>
           </div>
         </div>
       </li>
