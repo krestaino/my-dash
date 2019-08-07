1d93124f7ae9e336892de945d232a844b456982a
Kevin Restaino <kevinrestaino@gmail.com>
2019-08-07 17:53:38 -0500

    refactor: capitalize port

--- api/src/index.js
+++ api/src/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const cors = require('cors');
 
 const authenticate = require('./auth.js');
-const port = process.env.API_PORT;
+const PORT = process.env.API_PORT;
 const app = express();
 
 app.use(cors({ origin: process.env.UI_ORIGIN }));
 
 require('./routes.js')(app, authenticate, axios);
 
-app.listen(port, () => console.log('API: Accepting connections at http://localhost:' + port));
+app.listen(PORT, () => console.log('API: Accepting connections at http://localhost:' + PORT));
