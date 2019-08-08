5acb7e848da8a3583027e9405da931e4e1cacb53
Kevin Restaino <kevinrestaino@gmail.com>
2019-08-07 20:12:11 -0500

    wip(netdata): expand more info

--- ui/src/components/services/Netdata.js
+++ ui/src/components/services/Netdata.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import api from '../../api.js';
 import Loading from '../Loading.js';
 
+import { ReactComponent as IconChevronDown } from '../../assets/svg/chevron-down-solid.svg';
+
 export default class Netdata extends Component {
   state = {
     netdataDo: null,
@@ -33,12 +35,12 @@ export default class Netdata extends Component {
           {!netdataDo ? (
             <Loading response={netdataDo} />
           ) : (
-            <NetdataInstance data={netdataDo} url={process.env.REACT_APP_NETDATA_DO_URL} />
+            <NetdataInstance data={netdataDo} url={process.env.REACT_APP_NETDATA_DO_URL} id="do" />
           )}
           {!netdataHome ? (
             <Loading response={netdataHome} />
           ) : (
-            <NetdataInstance data={netdataHome} url={process.env.REACT_APP_NETDATA_HOME_URL} />
+            <NetdataInstance data={netdataHome} url={process.env.REACT_APP_NETDATA_HOME_URL} id="home" />
           )}
         </ul>
       </div>
@@ -47,8 +49,16 @@ export default class Netdata extends Component {
 }
 
 class NetdataInstance extends Component {
+  componentDidMount() {
+    const { id } = this.props;
+
+    document.querySelector(`button[data-trigger='${id}']`).addEventListener('click', () => {
+      document.querySelector(`[data-target='${id}']`).classList.toggle('hidden');
+    });
+  }
+
   render() {
-    const { data, url } = this.props;
+    const { data, id, url } = this.props;
 
     return (
       <li className="box mb-8" key={data.uid}>
@@ -71,6 +81,25 @@ class NetdataInstance extends Component {
           <div className="text-gray-600 dark:text-gray-500 text-sm justify-between flex w-full">
             <span>Normal</span>
             <span className="text-green-600">{data.alarms.normal}</span>
+          </div>
+          <div className="border-t dark:border-gray-700 mt-4 -m-4 p-4 hidden target" data-target={id}>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus lectus id velit scelerisque, vel
+              mattis velit fringilla. Integer et porttitor sem. Donec nulla neque, ullamcorper at dui ac, iaculis
+              malesuada lacus. In bibendum non purus et molestie. Pellentesque convallis sollicitudin elit, ac mattis
+              lorem volutpat vel. Aenean id lacus quis ex laoreet fringilla eu quis nunc. Vivamus non accumsan felis, ac
+              ullamcorper nisl.
+            </div>
+          </div>
+          <div className="border-t dark:border-gray-700 mt-4 -m-4 cursor-pointer">
+            <button
+              className="px-4 py-2 justify-between flex w-full items-center focus:outline-none text-sm text-gray-600 dark:text-gray-500"
+              data-trigger={id}
+              title="Expand"
+            >
+              <span>More info</span>
+              <IconChevronDown className="w-3" />
+            </button>
           </div>
         </div>
       </li>
