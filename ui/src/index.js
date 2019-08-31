9739821771e6aa87983a4cbb33cbb5b7fa2958ac
krestaino <kevinrestaino@gmail.com>
2019-08-31 18:20:39 -0500

    refactor: use PureComponent

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,7 +5,7 @@ import Auth from './components/Auth.js';
 import Services from './components/Services.js';
 import ThemeToggle from './components/ThemeToggle.js';
 
-class App extends React.Component {
+class App extends React.PureComponent {
   render = () => (
     <div>
       <ThemeToggle />
