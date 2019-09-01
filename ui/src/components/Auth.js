3dc6234c66bfcee7ec1d90aff93754c9460958f9
krestaino <kevinrestaino@gmail.com>
2019-09-01 16:29:04 -0500

    chore: add comments to auth

--- ui/src/components/Auth.js
+++ ui/src/components/Auth.js
@@ -18,25 +18,39 @@ export default class Auth extends Component {
   inputRef = React.createRef();
 
   handleSubmit = async event => {
+    // On auth form submission, grab the API_KEY from the
+    // text input and authenticate it.
     event.preventDefault();
     this.auth(this.inputRef.current.value);
   };
 
   auth = async API_KEY => {
     try {
+      // Show loader.
       this.setState({ loading: true });
+      // Set API_KEY in localStorage. Each request to the api uses this
+      // localStorage key, so we need to make sure to set it.
       window.localStorage.setItem('API_KEY', API_KEY);
+
+      // The api will respond with 200 if the API_KEY is correct.
       await api(process.env.REACT_APP_AUTH_ENDPOINT);
+
+      // If no error, set the API_KEY in state to render the Services
+      // and hide the auth form.
       this.setState({ API_KEY });
     } catch (error) {
+      // The api most likely rejected the key, remove it and reset API_KEY state
+      // to show the auth form again. The error will also be displayed.
       window.localStorage.removeItem('API_KEY');
       this.setState({ API_KEY: undefined, error: error.toString() });
     } finally {
+      // Hide loader.
       this.setState({ loading: false });
     }
   };
 
   componentDidMount = () => {
+    // If API_KEY is set in localStorage, authenticate it.
     const API_KEY = window.localStorage.getItem('API_KEY');
 
     if (API_KEY) {
@@ -47,10 +61,12 @@ export default class Auth extends Component {
   render() {
     const { API_KEY, loading, error } = this.state;
 
+    // Show loader while authenticating.
     if (loading) {
       return <Loading />;
     }
 
+    // If API_KEY is not set, render form to enter it.
     if (!API_KEY) {
       return (
         <div className="container mx-auto max-w-lg py-8 px-4">
@@ -78,6 +94,7 @@ export default class Auth extends Component {
       );
     }
 
+    // If not loading and API_KEY is set, render the Services.
     return this.props.children;
   }
 }
